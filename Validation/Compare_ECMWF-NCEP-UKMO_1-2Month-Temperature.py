705fbde8434bc0b0c5e5e1e7b976be48a9e90f80
Dr Christopher Nankervis <chris@weatherlogistics.com>
2024-01-20 22:21:45 +0000

    Update Compare_ECMWF-NCEP-UKMO_1-2Month-Temperature.py

--- Validation/Compare_ECMWF-NCEP-UKMO_1-2Month-Temperature.py
+++ Validation/Compare_ECMWF-NCEP-UKMO_1-2Month-Temperature.py
@@ -68,12 +68,12 @@ for supplier in suppliers:
         for lead_time in lead_times:
         
             # We want forecast for month ahead, we chose first element of our dataset
-            first_month = ds["t2m"][lead_time-1]
+            first_month = ds["t2a"][lead_time-1]
             # Transform 0 to 360 degree long          itudes, to longitude
             first_month["longitude"] = (first_month["longitude"] + 180) % 360 - 180
             first_month = first_month.sortby(first_month.longitude)
             # Create plot and set up basemap
-            plt.figure(dpi=350)
+            plt.figure(dpi=350, figsize=(10, 10))
             ax = plt.axes(projection=ccrs.Mercator(), frameon=True)
             # Set coordinate system of data and change extent of a map
             data_crs = ccrs.PlateCarree()
@@ -85,7 +85,9 @@ for supplier in suppliers:
             ax.add_feature(cf.BORDERS.with_scale("50m"), lw=0.3) # Add coastlines
             lonsi, latsi = np.meshgrid(first_month["longitude"], first_month["latitude"]) # Create grid from latitude and longitude
             # Plot the data as filled contour
-            cs = ax.contourf(lonsi, latsi, values, transform=data_crs, levels=128, cmap="seismic_r", vmin=-0.000000045, vmax=0.000000045)
+            vmin = 0.0; vmax = 3.0
+            levels = np.linspace(vmin, vmax, 32+1)
+            cs = ax.contourf(lonsi, latsi, values, transform=data_crs, levels=levels, cmap="seismic", vmin=vmin, vmax=vmax)
             # Get attributes needed for title
             name = first_month.attrs["GRIB_name"]
             units = first_month.attrs["GRIB_units"]
@@ -93,10 +95,11 @@ for supplier in suppliers:
             valid_date = pd.to_datetime(valid_date)
             valid_date = valid_date.strftime("%B %Y")
             plt.suptitle(supplier.upper(), y=1.05, fontsize=18)
-            plt.title(f"{name} ({units}) {valid_date}")
+            plt.title(f"{name}\n({units}) {valid_date}")
             plt.colorbar(cs,orientation="horizontal",ax=ax,
                         pad=0,
                         aspect=50)
             
             # Save Plot
             plt.savefig(f'raw_data/comparison_{start_month}_{start_year}_{lead_time}_{supplier}_Temperature.png', dpi=350)
+    
